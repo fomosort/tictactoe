b2cb16888ce2c7ab87b3534d04f925144ae6c011
binudrinker <56517440+fomosort@users.noreply.github.com>
2023-08-23 19:41:34 -0700

    Add ability to click to place pieces, swap players

--- script.js
+++ script.js
@@ -26,14 +26,55 @@ const displayController = ((board) => {
   };
 
   const updateBoardHTML = (row, column, pieceXO) => {
-    const selectedSquare = boardElem.querySelector(`.row${row}.col${column}`);
+    const selectedSquare = boardElem.querySelector(
+      `.square[row="${row}"][col="${column}"]`
+    );
     selectedSquare.textContent === "_"
       ? (selectedSquare.textContent = pieceXO)
       : console.log("Square is occupied");
   };
-  return { initBoardHTML, updateBoardHTML };
+
+  const takePlayerInput = (player) => {
+    allSquares.forEach((square) =>
+      square.addEventListener("click", function(){
+    const positionToMove = [this.getAttribute("row"),this.getAttribute("col")]
+        currentPlayer.playPiece(positionToMove);
+        changeCurrentPlayer()
+      })
+    );
+  };
+  return { boardElem, initBoardHTML, updateBoardHTML, takePlayerInput };
 })(board);
+
+const Player = (piece) => {
+  const info = {
+    score: 0,
+    pieceType: piece,
+  };
+
+  const playPiece = (playPosition) => {
+    gameBoard.updateBoard(...playPosition,info.pieceType);
+    displayController.updateBoardHTML(...playPosition, info.pieceType)
+  };
+
+  return { info, playPiece };
+};
+
+// const mainGame = (() =>{
+//Create players
+const humanPlayer = Player("X");
+const cpuPlayer = Player("O");
+let currentPlayer = humanPlayer;
+const changeCurrentPlayer = () =>{
+    currentPlayer = currentPlayer === humanPlayer ? cpuPlayer : humanPlayer 
+    console.log(currentPlayer);
+}
+
+// })()
+
+// console.log(document.querySelector(`.square[row="1"][col="1"]`));
 
 displayController.initBoardHTML(gameBoard.board);
 displayController.updateBoardHTML(0, 2, "O");
 displayController.updateBoardHTML(0, 1, "X");
+displayController.takePlayerInput(humanPlayer);
